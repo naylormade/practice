fe0edbf328f27710d163d76acb7a519a2a9519f8
Daniel Naylor <naylordaniel@yahoo.com>
2019-11-08 12:55:33 -0800

    like 2 lines for is_valid '-'

--- is_valid.py
+++ is_valid.py
@@ -2,7 +2,14 @@ def isValid(s: str) -> bool:
     if s == "": return "true"
 
     if len(s) % 2 != 0:
-        return "False"
+        print("False")
+        return False
     aslist = list(s)
     print(f'{aslist}')
+
+    for ch in aslist:
+        print(f'{ch}')
     return False
+
+if __name__ == "__main__":
+    isValid("()[]{}")
