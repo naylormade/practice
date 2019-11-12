a3d3fec582f9d1562801d0c789b23130749bd20e
Daniel Naylor <naylordaniel@yahoo.com>
2019-11-12 13:34:58 -0800

    longest prefix, want to test another idea

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -1,10 +1,23 @@
 def longestCommonPrefix(strs) -> str:
     ppref = ""
-    indexes = 0
     
-    if not strs: return ""
-    else: first_word = strs[0]
-    
+    if not strs: return ""  #empty string
+    if len(strs) == 1: return strs[0]  #only 1 given string
+    if strs: first_word = strs[0]  #initialize first word
+    if first_word == "":  #if given an empty string but empty
+        return ""
+    for word in strs[1:]:
+        indexes = 0
+        for letter in word:
+            if letter == first_word[indexes]:
+                ppref += letter
+                indexes += 1
+            else:
+                print("No match on 2nd word so ending")
+                return ""
+    if ppref:
+        print(f'ppref: {ppref}')
+        return ppref
     
 
-#longestCommonPrefix(["aa","aa"])
+longestCommonPrefix(["flower","flow","flight"])
