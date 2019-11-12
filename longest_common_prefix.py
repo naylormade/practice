9dd48c16bce5d46cbfbea73bd7767b9386a0e0e5
Daniel Naylor <naylordaniel@yahoo.com>
2019-11-12 14:18:37 -0800

    progress thus far

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -1,23 +1,31 @@
 def longestCommonPrefix(strs) -> str:
-    ppref = ""
+    #ppref = ""
     
     if not strs: return ""  #empty string
     if len(strs) == 1: return strs[0]  #only 1 given string
-    if strs: first_word = strs[0]  #initialize first word
-    if first_word == "":  #if given an empty string but empty
+    if strs: ppref = strs[0]  #initialize first word
+    if ppref == "":  #if given an empty string but empty
         return ""
+
+    ans = ""
     for word in strs[1:]:
         indexes = 0
         for letter in word:
-            if letter == first_word[indexes]:
-                ppref += letter
+            if letter == ppref[indexes]:
+                ans += letter
                 indexes += 1
             else:
+                if ans:
+                    ans = ppref[:indexes]
+                    print(f'ans:{ans}')
+                    return ans
                 print("No match on 2nd word so ending")
                 return ""
-    if ppref:
-        print(f'ppref: {ppref}')
-        return ppref
+    if ans:
+        print(f'ans: {ans}')
+        return ans
+    else:
+        return ""
     
 
-longestCommonPrefix(["flower","flow","flight"])
+longestCommonPrefix(["a","a","a"])
