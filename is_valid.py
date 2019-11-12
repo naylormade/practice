1bc80bc99e6f7bb83a4896b218a5c2a07e155c99
Daniel Naylor <naylordaniel@yahoo.com>
2019-11-12 09:47:51 -0800

    finished!

--- is_valid.py
+++ is_valid.py
@@ -1,24 +1,22 @@
 def isValid(s: str) -> bool:
-    if s == "": return "true"
-
     if len(s) % 2 != 0:
-        print("False")
         return False
-    #aslist = list(s)
+
     maps = {
         ")": "(", 
         "]": "[",
         "}": "{"
     }
     stackx = []
-    for ch in list(s):
-        #print(f'{ch}')
-        if ch == "(" or ch == "[" or ch == "{":
+    for ch in s:
+        if ch in maps:
+            last_element = stackx.pop() if stackx else ""
+            
+            if maps[ch] != last_element:
+                return False
+        else:  #if we encounter an opening bracket
             stackx.append(ch)
-        else:  #if we encounter a closing bracket
-            if (stackx[-1]) == maps[ch]:
-                stackx.pop()
-    return True
+    return not stackx
 
 if __name__ == "__main__":
-    isValid("()")
+    isValid("(((((((()")
