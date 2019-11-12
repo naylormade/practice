7c245420e7ed049d6139f9d367604645157172c7
Daniel Naylor <naylordaniel@yahoo.com>
2019-11-12 09:00:10 -0800

    added some logic

--- is_valid.py
+++ is_valid.py
@@ -4,12 +4,21 @@ def isValid(s: str) -> bool:
     if len(s) % 2 != 0:
         print("False")
         return False
-    aslist = list(s)
-    print(f'{aslist}')
-
-    for ch in aslist:
-        print(f'{ch}')
-    return False
+    #aslist = list(s)
+    maps = {
+        ")": "(", 
+        "]": "[",
+        "}": "{"
+    }
+    stackx = []
+    for ch in list(s):
+        #print(f'{ch}')
+        if ch == "(" or ch == "[" or ch == "{":
+            stackx.append(ch)
+        else:  #if we encounter a closing bracket
+            if (stackx[-1]) == maps[ch]:
+                stackx.pop()
+    return True
 
 if __name__ == "__main__":
-    isValid("()[]{}")
+    isValid("()")
