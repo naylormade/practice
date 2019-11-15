956ce7cd51c6ff7aceb91379a4c32073d6142ebe
Daniel Naylor <naylordaniel@yahoo.com>
2019-11-15 08:02:45 -0800

    finished - faster than 97.52% of solutions

--- reverse.py
+++ reverse.py
@@ -1,34 +1,16 @@
 def reverse( x: int) -> int:
-    joined_list = ''.join(list(reversed(str(x)[:-1])))
-    #print(joined_list)
-    if str(x)[0] == "-":
-        print("-" + ''.join(list(reversed(str(x)[1:]))))
-        return "-" + ''.join(list(reversed(str(x)[1:])))
-    else:
-        if joined_list[0] != str(0):
-            print(''.join(list(reversed(str(x)))))
-            return ''.join(list(reversed(str(x))))
-        else: 
-            print(''.join(list(reversed(str(x)[1:]))))
-            return ''.join(list(reversed(str(x)[1:])))
-        
-reverse(120)
-#reverse(-123)
-
-
-def reverse2( x: int) -> int:
     ans = ""
     if len(str(x)) == 1: return x
-    testt = list(reversed(str(x)))
-    #print(f'testt[0]:{testt[0]}')
-    if testt[0] == str(0):
-        #print(f'first: {"".join(testt[1:])}')
-        ans = ''.join(testt[1:])
-    if testt[-1] == "-":
-        print("2nd")
-        #print("-" + ''.join(testt))
-        #print("-" + ans)
+    ans = ''.join(list(reversed(str(x))))
+    while ans[0] == str(0):
+        ans = ''.join(ans[1:])
+    if ans[-1] == "-":
         ans = "-" + ans[:-1]
-    #print(f'3rd: {"".join(testt)}')
+    print(f'ans:{ans}')
+    if abs(int(ans)) > (2 ** 31 - 1):
+        return 0
     return ans
-reverse2(-10)
+
+# reverse(100)
+# reverse(-10)
+# reverse(123)
