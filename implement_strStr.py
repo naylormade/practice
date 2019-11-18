34c92717278b54ba763255a7ad65239cb8e26ffd
Daniel Naylor <naylordaniel@yahoo.com>
2019-11-18 14:19:06 -0800

    finished implement_strStr

---
+++ implement_strStr.py
@@ -0,0 +1,6 @@
+def strStr(haystack: str, needle: str) -> int:
+        if not needle:
+            return 0
+        if needle in haystack:
+            return haystack.index(needle)
+        return -1
