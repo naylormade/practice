87ace36747bffeb411eeba1c3d4b21f7bc666baa
Daniel Naylor <naylordaniel@yahoo.com>
2019-11-19 13:23:21 -0800

    working through fringe cases

--- lengthOfLastWord.py
+++ lengthOfLastWord.py
@@ -1,4 +1,14 @@
 def lengthOfLastWord(s: str) -> int:
-        if len(s.split(" ")) == 1:
-            return len(s)
-        return len(s.split(" ")[-1])
+    splitz = s.split(" ")
+    if len(''.join(splitz).strip()) == 1:
+        #print("1")
+        return 1
+    print(splitz)
+    while(len(splitz[-1])) == 0:
+        #print(f'ans: {len(splitz[-2])}')
+        print(f'splitz:{splitz}')
+        splitz.pop()
+    print(f'ans: {len(splitz[-1])}')
+    return len(splitz[-1])
+
+lengthOfLastWord("b a ")
