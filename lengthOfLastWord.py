4f360d437d869a47ff27523262829fbbb5c06ca5
Daniel Naylor <naylordaniel@yahoo.com>
2019-11-20 07:48:26 -0800

    finished plusOne

--- lengthOfLastWord.py
+++ lengthOfLastWord.py
@@ -1,16 +1,13 @@
 def lengthOfLastWord(s: str) -> int:
     if not s or s.strip() == 0: return 0
     splitz = s.split(" ")
+
     if len(''.join(splitz).strip()) == 1:
-        #print("1")
         return 1
-    print(splitz)
+
     while len(splitz[-1]) == 0 and len(splitz) > 1:
-        #print(f'ans: {len(splitz[-2])}')
-        print(f'splitz[-1]:{splitz[-1]}')
         splitz.pop()
 
-    print(f'ans: {splitz[-1]}')
     if splitz:
         return len(splitz[-1])
     else:
--- plusOne.py
+++ plusOne.py
@@ -1,2 +1,8 @@
-def plusOne(digits: List[int]) -> List[int]:
-        return 1
+def plusOne(digits):
+    nums = ""
+    for dig in digits:
+            nums += str(dig)
+    ans = int(nums) + 1
+    print(list(str(ans)))
+    return list(str(ans))
+plusOne([1,2,3])
