738f58c41e6ebef29c8758e09e54f123ef15c247
Daniel Naylor <naylordaniel@yahoo.com>
2019-11-20 07:57:02 -0800

    finished with a 1 liner but a bit slower

--- plusOne.py
+++ plusOne.py
@@ -1,8 +1,11 @@
 def plusOne(digits):
+    return list(str(int(''.join([str(x) for x in digits])) + 1))
+plusOne([1,2,3])
+
+def plusOneAlternateMethod(digits):
     nums = ""
     for dig in digits:
             nums += str(dig)
     ans = int(nums) + 1
-    print(list(str(ans)))
+    #print(list(str(ans)))
     return list(str(ans))
-plusOne([1,2,3])
